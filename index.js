be070e3b695880a79171f5237f5f62a5cbff9098
Franco Sasieta <fsasieta@users.noreply.github.com>
2016-10-15 14:29:41 -0700

    different approach

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 var express = require('express')
+var http = require("http");
+var fs   = require("fs");
+var path = require("path");
+var mime = require("mime");
+
 
 var app = express();
 
-app.get('/events_available', function(req, res) {
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
 
-	
-	res.json({event1: "Comm working in browser"})
-	
-	//get_list_of_events()
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
 
-})
+var server = http.createServer(function(request, response) {
+  var filePath = false;
 
-app.listen(3000);
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+var port_number = server.listen(process.env.PORT || 3000);
 
 
-
+/*
 var get_list_of_events = function () {
-
-	var fs = require("fs");
 
 	var json_data = fs.readfile('input.txt', function(err, data) {
 
@@ -33,4 +66,15 @@ var get_list_of_events = function () {
 	console.log(json_data);
 
 }
+*/
+
+/*
+app.get('/events_available', function(req, res) {
+	res.json({event1: "Comm working in browser"})
+	//get_list_of_events()
+})
+
+app.listen(3000);
+*/
+
 
